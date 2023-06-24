4e165162e14ca26336c00a1eeda2e979b0eeb606
syedsimanta03 <syed.simanta10@gmail.com>
2023-06-24 16:45:03 +0000

    fav

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '../global.css'
+import Head from 'next/head'
+
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>simanta-sb5</title>
+        <link rel='icon' href='/android-chrome-192x192.png' />
+      </Head>
+      {/* Rest of your app content */}
+      <Component {...pageProps} />
+    </>
+  )
+ 
 }
