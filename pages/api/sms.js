80c37d7453245fd943b7cbc6f17c32bd7c5a6db1
syedsimanta03 <syed.simanta10@gmail.com>
2023-06-26 19:12:48 +0000

    pp

--- pages/api/sms.js
+++ pages/api/sms.js
@@ -1,9 +1,9 @@
 import twilio from 'twilio'
-//package not taking env variables
+
 export default async function handler(req, res) {
   const client = twilio(
-    'ACcf49d743b4efa9b9796be98f4c02fb09',
-    '7733e12217ff94fb6e5caa3b573a55ce'
+    process.env.NEXT_PUBLIC_T_ACCOUNT_SID,
+    process.env.NEXT_PUBLIC_T_AUTH_TOKEN
   )
   
 
